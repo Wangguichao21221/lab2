d63bf7241129a4929d3e8bd63fc41654816330b5
Wangguichao21221 <wanggch2023@shanghaitech.edu.cn>
2025-03-03 12:48:09 +0800

    another queue free in queue.h

--- queue.h
+++ queue.h
@@ -14,4 +14,5 @@ Queue *queue_create(void);
 void push(Queue *queue, double element);
 double back(Queue *queue);
 void queue_free(Queue *queue);
+void another_queue_free(Queue **);
 #endif // LAB_2_QUEUE_H
